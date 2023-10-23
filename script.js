b266a9ee0779cb794878728694eb232cbbcaa483
Samuel Gomez <gocs020101@gs.utm.mx>
2023-10-22 19:07:22 -0600

    Gravity added

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-var canvas = document.getElementById("animacion");
-var ctx = canvas.getContext('2d');
+const canvas = document.getElementById("animacion");
+const ctx = canvas.getContext('2d');
 
-var wings_down = new Image();
-var wings_up = new Image();
+const wings_down = new Image();
+const wings_up = new Image();
 wings_down.src =  "pngegg (3).png";
 wings_up.src = "pngegg (4).png";
 
-var flap = true; // Variable to alternate beetwen pictures
+let flap = true; // Variable to alternate beetwen pictures
 
 // Counter
 // Remember when i++ bird go down
 //               i-- bird go up
 //var i = 0
 
-y = canvas.height/2-wings_down.height/2;
+let y = canvas.height/2-wings_down.height/2;
+let gravity = 4;
+let isKeyPressed = false;
 
 document.addEventListener("keydown", (e) => {
     if(e.key == "ArrowUp") {
         y -= 20;
+        isKeyPressed = true;
     } else if (e.key == "ArrowDown") {
         y += 20;
+        isKeyPressed = true;
     }
 });
 
+document.addEventListener("keyup", (e) => {
+    isKeyPressed = false; 
+});
+
+
 wings_down.onload = function() {
+    startGame();
+}
+
+function startGame() {
     // y : coordinate y bird
     var intervalID = setInterval(function() {
+        // Gravity is actived when !isKeyPressed
+        if (!isKeyPressed) {
+            y += gravity;
+        }
         // Indicates if there is a collision
         if (y + wings_up.height >= canvas.height + 5) {
             clearInterval(intervalID);
+            console.log("Game Over!");
         } else if (y + wings_up.height < 0 || y + wings_down.height < 0) {
             
         }
         draw(y);
-        }, 100);
+    }, 100);
 }
 
 // Changes in orden to y
@@ -46,6 +64,7 @@ function draw(y) {
     }
     flap = !flap; // Alternate beetwen the state var
 }
+
 
 /*
 function draw_text(text) {
